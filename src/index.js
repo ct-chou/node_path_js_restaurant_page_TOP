d99cd53a0f329826dc9a8b09468c756bb9060bc3
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-10-22 09:45:49 -0400

    Feat: added  npm support for css and temporarily comment out using home.js instead hardcode into template.html. Work on making the hardcoding look nice before replicating using DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './styles.css';
 import { header } from './header.js';
 import { home } from './home.js';
 document.body.appendChild(header);
-header.insertAdjacentElement('afterend', home);
+// header.insertAdjacentElement('afterend', home);
 
 console.log('Welcome!');
+// add footer attribution to www.freepik.com
